c1888e88b6d6e3bc4d9d974f05b56ab6a9fa4121
Laura <lauracorssac@gmail.com>
2019-11-15 18:31:22 -0300

    initial files that come automatically

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/lauracorssac/Downloads/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/lauracorssac/AndroidStudioProjects/meeting_friends"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/lauracorssac/Downloads/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
