881ea1784147cdb8e7e7dce5fb5f493ba2e79ff5
Laura <lauracorssac@gmail.com>
2019-11-23 17:13:05 -0300

    update on meeting details screen

--- lib/MeetingDetails.dart
+++ lib/MeetingDetails.dart
@@ -16,6 +16,66 @@ class MeetingDetails extends StatelessWidget {
       appBar: AppBar(
           title: Text(_meeting.title)
       ),
+      body:
+          ListView(
+            children: [
+              Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: Padding(
+                        padding: EdgeInsets.all(10),
+                        child: Text(
+                            _meeting.title,
+                            style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 24,
+                                  fontWeight: FontWeight.bold)
+                        )
+                      ),
+                     )
+                  ]),
+              Row(
+                  children:[
+                    Expanded(
+                      child:
+                        Padding(
+                          padding: EdgeInsets.all(10),
+                          child: Text(_meeting.date,
+                            style: TextStyle(color: Colors.grey,
+                                fontSize: 20,
+                                fontWeight: FontWeight.normal)
+                        ),
+                      )
+                    )
+                  ]),
+              Divider(thickness: 1.0, color: Colors.grey),
+              Row(
+                  children: [
+                    Expanded(
+                        child: Padding(
+                            padding: EdgeInsets.all(10),
+                            child: Text(
+                            _meeting.description,
+                              style: TextStyle(
+                                  color: Colors.grey,
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.normal)
+                          )
+                        )
+                    )
+                  ]),
+              Divider(thickness: 1.0, color: Colors.grey),
+              Row(
+                  children: [
+                    Padding(padding: EdgeInsets.all(10),child: Icon(Icons.location_on, color: Colors.grey)),
+                    Expanded(
+                      child: Text(_meeting.location.toString(),
+                          style: TextStyle(color: Colors.grey, fontSize: 20)
+                      ),
+                    )
+
+                  ])
+            ])
     );
   }
 }
